d46cd0c597463ba81c188cd8924540045f218389
Kiryl Mudrakou <Kiryl_Mudrakou@epam.com>
2022-08-23 01:31:58 +0300

    task7 added tests done

--- build/src/constants/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_SECRET_KEY = void 0;
-exports.JWT_SECRET_KEY = 'secretJwt';
-//# sourceMappingURL=constants.js.map
--- build/src/middlewares/loger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.log = void 0;
-const log = function (req, res, next) {
-    console.log("\x1b[36m%s%s1\x1b[0m", '\npath: ', req.originalUrl, '\nbody:', req.body, '\nparams :', req.params, '\nqueryParams:', req.query, "\n");
-    next();
-};
-exports.log = log;
-//# sourceMappingURL=loger.js.map
