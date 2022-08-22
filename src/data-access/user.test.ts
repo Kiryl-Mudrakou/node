ee262804152f09fbd49c01541e085681c4fe2072
Kiryl Mudrakou <Kiryl_Mudrakou@epam.com>
2022-08-23 01:28:31 +0300

    task7 added tests done

--- src/data-access/user.test.ts
+++ src/data-access/user.test.ts
@@ -1,24 +1,57 @@
-import { findOne} from "./user";
+import { findOne, create, remove, update, findAll, getAll, findByLogin} from "./user";
 
+jest.mock('./user', () => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+  remove: jest.fn(),
+  update: jest.fn(),
+  findAll: jest.fn(),
+  getAll: jest.fn(),
+  findByLogin: jest.fn(),
+}))
+const user = {login: 'st', age: 19, id: 1, isDeleted: false, password:'124asd '};
 
-jest.mock('./user').
+describe("User", () => {
 
-describe("User Service", () => {
-  it("it should create a user", async () => {
-    const createUserMock = await findOne();
+  it("it should find user", async () => {
+    await findOne(1);
 
-    expect(createUserMock).toHaveBeenCalled();
+    expect(findOne).toHaveBeenCalledWith(1);
   });
-  // it("it should find a user by id", async () => {
-  //   let result = await UserService.getUser();
-  //   expect(result).toHaveBeenCalled();
-  // });
-  // it("it should mark as deleted by id", async () => {
-  //   let result = await userService.deleteUser(data.id);
-  //   expect(result.isdeleted).toBeTruthy();
-  // });
-  // it("it should find all the matched terms", async () => {
-  //   let result = await userService.searchUser(5,'shiva');
-  //   expect(result).toEqual([data]);
-  // });
+
+  it("it should create user", async () => {
+    await create(user);
+
+    expect(create).toHaveBeenCalledWith(user);
+  });
+
+  it("it should remove user", async () => {
+    await remove(1);
+
+    expect(remove).toHaveBeenCalledWith(1);
+  });
+
+  it("it should update user", async () => {
+    await update(1, user );
+
+    expect(update).toHaveBeenCalledWith(1, user);
+  });
+
+  it("it should findAll users", async () => {
+    await findAll('some', 1);
+
+    expect(findAll).toHaveBeenCalledWith('some', 1);
+  });
+
+  it("it should getAll users", async () => {
+    await getAll();
+
+    expect(getAll).toHaveBeenCalledTimes(1);
+  });
+
+  it("it should findByLogin user", async () => {
+    await findByLogin('name');
+
+    expect(findByLogin).toHaveBeenCalledWith('name');
+  });
 });
